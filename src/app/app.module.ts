162ae66048a193155f71552552ee5f551d91ccc1
ene99167788 <eneylton@hotmail.com>
2021-04-18 22:15:40 -0300

    nova Aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,52 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+
+import { HttpClientModule } from '@angular/common/http';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { IonicStorageModule } from '@ionic/Storage';
+import { ServiceProvider } from '../providers/service/service';
+
+import { Camera } from '@ionic-native/camera';
+
+import {LOCALE_ID} from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import {registerLocaleData} from '@angular/common';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
+
+registerLocaleData(localePt, 'pt');
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+   
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    HttpClientModule,
+    IonicStorageModule.forRoot(),
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    
   ],
   providers: [
     StatusBar,
+    Camera,
+    BarcodeScanner,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ServiceProvider,
+    {
+      provide: LOCALE_ID,
+      useValue: "pt"
+    }
   ]
 })
 export class AppModule {}
